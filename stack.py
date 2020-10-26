e4853cf2ae79d082bc20b466cd89ba2e7bdc5982
Ryan Earl <ryanleoearl@gmail.com>
2020-10-25 23:40:46 -0700

    Completed first two tests

--- stack.py
+++ stack.py
@@ -1,7 +1,14 @@
 # Stack: A stack.
 # Your implementation should pass the tests in stack.py.
-# YOUR NAME
+# Ryan Earl
 
 class Stack:
+    def __init__(self): 
+        self.capacity = 10
+        return None 
 
-    pass
+    def is_empty(self):
+        return True 
+
+    def pop(self):
+        return None
